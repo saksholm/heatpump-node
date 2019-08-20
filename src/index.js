d42cb98b00ba6366196829f2912402e3ef8ff9b4
Joni Saksholm <joni@saksholm.fi>
2019-08-20 14:14:34 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-// main
+import five from "johnny-five";
+import {
+  DI,
+  DO,
+  AI,
+  AO,
+} from './includes/io';
+const board = new five.Board();
+
+
+console.log("io's", DI,DO,AI,AO);
+
+DO.ahu_fan.on();
+
+/*
+
+board.on("ready", function() {
+
+  this.repl.inject({
+    on: input => {
+      console.log("on on on", typeof input, input);
+    },
+    off: input => {
+      console.log("off off off", input);
+    },
+  });
+
+
+
+  // clear stuff
+  this.on("exit", function() {
+    led.off();
+  });
+
+});
+*/
