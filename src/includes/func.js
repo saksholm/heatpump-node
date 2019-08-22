439cf9970b886776ccdb24cffd4dfddc56baf728
Joni Saksholm <joni@saksholm.fi>
2019-08-22 22:18:12 +0300

    Update func.js

--- src/includes/func.js
+++ src/includes/func.js
@@ -1,30 +1,9 @@
-const initialized = (io, text) => {
-  console.log(`DO, ${text} initialized`);
-};
 
-export class Initialized {
-  constructor(io) {
-    this.io = io;
-  }
 
-  done (text) {
-    console.log(`${this.io}, ${text}... DONE`);
-  }
-}
 
-class Foo {
-    constructor(x) {
-        this.x = x;
-    }
-
-    point() {
-        return 'Foo(' + this.x + ')';
-    }
-}
 
 
 
 export {
-  initialized,
-  Foo,
+
 };
