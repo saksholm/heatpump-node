f3dd4ad1af7f9feed942d4cf823bdb390e2cbaa6
Joni Saksholm <joni@saksholm.fi>
2019-08-26 23:40:17 +0300

    Update func.js

--- src/includes/func.js
+++ src/includes/func.js
@@ -1,5 +1,12 @@
+import getUnixTime from 'date-fns/getUnixTime';
 import {GLOBALS} from './globals';
 
 export const parseMQTTString = path => {
   return `${GLOBALS.mqttBase}${path}`;
+};
+
+export const unixtimestamp = (datetime=null, microtime=false) => {
+  if(!datetime) datetime = new Date();
+  if(microtime) return getUnixTime(datetime * 1000);
+  return getUnixTime(datetime);
 };
