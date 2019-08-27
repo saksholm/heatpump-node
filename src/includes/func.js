af13a420a78c782359890ac77811ce798083d616
Joni Saksholm <joni@saksholm.fi>
2019-08-27 23:26:42 +0300

    Update func.js

--- src/includes/func.js
+++ src/includes/func.js
@@ -1,5 +1,11 @@
+import five from 'johnny-five';
 import getUnixTime from 'date-fns/getUnixTime';
 import {GLOBALS} from './globals';
+
+const {
+  map,
+  constrain,
+} = five.Fn;
 
 export const parseMQTTString = path => {
   return `${GLOBALS.mqttBase}${path}`;
@@ -9,4 +15,21 @@ export const unixtimestamp = (datetime=null, microtime=false) => {
   if(!datetime) datetime = new Date();
   if(microtime) return getUnixTime(datetime * 1000);
   return getUnixTime(datetime);
+};
+
+export const calculateTimeout = (timestamp, delay, milliseconds=false) => {
+  let timeout;
+
+  if( timestamp === 0) timeout = 0;
+  if(timestamp !== 0) {
+    if( timestamp + delay <= unixtimestamp() ) timeout = 0;
+    if( unixtimestamp() - timestamp <= delay)  timeout = (timestamp + delay) - unixtimestamp();
+  }
+  if(milliseconds) return timeout * 1000;
+  return timeout;
+};
+
+export const mapPercentToPWM = value => {
+  value = constrain(value, 0, 100);
+  return map(value, 0,100, 0,255);
 };
