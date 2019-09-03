0adc9286560bc61514dc14abbf566cf1c28b4799
Joni Saksholm <joni@saksholm.fi>
2019-09-03 10:01:24 +0300

    capitalize io const

--- src/includes/io.js
+++ src/includes/io.js
@@ -1,5 +1,3 @@
-//import five from 'johnny-five';
-//const board = new five.Board();
 import {GLOBALS} from './globals';
 import {DO} from './do';
 import {DI} from './di';
@@ -8,12 +6,10 @@ import {AI} from './ai';
 import {TH} from './th';
 import {HP} from './hp';
 
-const io = {
-  
-};
+const IO = {};
 
 
-io.initial = board => {
+IO.initial = board => {
   console.log("Start initialising io's");
   DO.initial(board);
   DI.initial(board);
@@ -23,6 +19,4 @@ io.initial = board => {
   HP.initial(board);
 };
 
-
-
-export {io};
+export {IO};
