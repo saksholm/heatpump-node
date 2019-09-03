19cab7c98e184a6d830d7372254344c59585798e
Joni Saksholm <joni@saksholm.fi>
2019-09-03 10:02:31 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,38 +1,37 @@
-const five = require("johnny-five");
-var mqtt = require('mqtt')
+import five from 'johnny-five';
+import mqtt from 'mqtt';
 
-
-import {io} from './includes/io';
 import {GLOBALS} from './includes/globals';
-import {secrets} from './secrets.js';
+import {IO} from './includes/io';
+import {SECRETS} from './secrets.js';
 import {DO} from './includes/do';
 import {DI} from './includes/di';
 import {AO} from './includes/ao';
 import {AI} from './includes/ai';
-import {logic} from './includes/logic';
+import {LOGIC} from './includes/logic';
+import {HP} from './includes/hp';
+
 import {
   unixtimestamp,
   mqttSubscriptions,
   mqttOnMessage,
   mqttCommandTopics,
 } from './includes/func';
-import {HP} from './includes/hp';
 
 const {
   mqttServer,
   mqttUser,
   mqttPass,
-} = secrets;
+} = SECRETS;
 
 const mqttClient = mqtt.connect(mqttServer, {username: mqttUser, password: mqttPass});
 const board = new five.Board({timeout: 3600});
 
-mqttClient.commandTopics = mqttCommandTopics();
 
-board.mqttClient = mqttClient;
+mqttClient.commandTopics = mqttCommandTopics(); // create dynamic mqttCommandTopics array
+board.mqttClient = mqttClient; // inject mqttClient to board instance
 
 console.log(unixtimestamp());
-
 
 board.on("ready", function() {
 
@@ -41,27 +40,22 @@ board.on("ready", function() {
     GLOBALS.starting = false;
   });
 
-  io.initial(this);
-
+  IO.initial(this);
   HP.start();
 
   this.repl.inject({
     info: () => {
       console.log("DO ahuFan", DO.ahuFan);
-//      DO.ahuFan.initial(five);
-
     },
-
+    // test repl
     ledOutput: value => {
       DO.ahuFanOutput.set(value);
     },
   });
 
-
   // clear stuff
-
   this.on("exit", function() {
-    // TODO: shutdown everything necessary!!
+    // TODO: shutdown everything!!
   });
 
 });
@@ -76,7 +70,6 @@ mqttClient.on('connect', function() {
 
   mqttSubscriptions(mqttClient);
 
-
 //  mqttClient.subscribe('cmnd/iot/heatpump/hp/output');
 
   mqttClient.subscribe('state/iot/heatpump/online', (err) => {
@@ -89,8 +82,6 @@ mqttClient.on('connect', function() {
 mqttClient.on('message', (topic, message) => {
   console.log("topic",topic.toString());
   console.log("message",message.toString());
-//  mqttClient.end();
-
 
   mqttOnMessage(mqttClient,topic,message);
 
