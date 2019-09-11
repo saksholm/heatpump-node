d620c17e3891b6e99ce98a1dd7b39b921a7b9317
Joni Saksholm <joni@saksholm.fi>
2019-09-11 23:52:56 +0300

    Analog Input updates.. condenserPDE added, AI.onChanges watchers

--- src/ai.js
+++ src/ai.js
@@ -1,20 +1,61 @@
+import five from 'johnny-five';
 import {
   Initialized,
 } from './initialized.class';
-import {genericInitial} from './func';
+import {GLOBALS} from './globals';
+import {
+  genericInitial,
+  mqttPublish,
+} from './func';
 
 const initialized = new Initialized('AI');
 
+const {
+  Pin,
+} = five;
+
 export const AI = {
   board: null,
-  AI1: 'AI1',
+  interval: 2*1000,
+  threshold: 2,
+  condenserPde: {
+    type: 'pde',
+    name: 'Condenser PDE',
+    pin: 'A0',
+    pinMode: Pin.INPUT,
+    value: 0,
+    set: function(value) {
+      this.value = value;
+    },
+    meltingPa: 40, // ?
+    limitingPa: 50, // ?
+    warningPa: 60, // ?
+    mqttCommand: '',
+    mqttState: '',
+    output: null,
+    initial: function() {
+      this.output = new five.Sensor({pin: this.pin, freq: AI.interval, threshold: AI.threshold}),
+      initialized.done(this.name);
+    },
+  },
 
-
-  /*
-
-  pinMode(AI_CONDENSER_PA, INPUT); // Pa over Condenser --- for melting purpose or limiting stuff...?
-
-  */
 };
 
-AI.initial = board => genericInitial(AI, 'AI', board);
+AI.onChanges = () => {
+  console.log("Mapping AI onChanges");
+  Object.keys(AI).map(key => {
+    const instance = AI[key];
+    if(key !== "board" && typeof instance !== null && typeof instance === 'object') {
+      if(instance.output !== null) {
+        instance.output.on("change", function(value){
+          instance.set(value);
+          if(GLOBALS.debug) console.log(`${instance.name} value changed to ${value}`);
+        });
+        console.log(`AI, ${instance.name} onChanges watchers activated.... DONE`);
+      }
+    }
+  });
+};
+
+
+AI.initial = board => genericInitial(AI, 'AI', board, AI.onChanges);
