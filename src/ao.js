8848dcf2561f3f0206d05c0d1cb0bc643ffdab61
Joni Saksholm <joni@saksholm.fi>
2019-10-11 15:51:00 +0300

    Update ao.js

--- src/ao.js
+++ src/ao.js
@@ -1,16 +1,14 @@
 import {
   Initialized,
 } from './initialized.class';
-import {genericInitial} from './func';
+//import {genericInitial} from './func';
 
 const initialized = new Initialized('AO');
 
 const AO = {
-  board: null,
-  AO1: 'AO1',
 };
 
-AO.initial = board => genericInitial(AO, 'AO', board);
+//AO.initial = board => genericInitial(AO, 'AO', board);
 
 
 
