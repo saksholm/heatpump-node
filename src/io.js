11aa88cc8599635abfcd9a11a25a325258249949
Joni Saksholm <joni@saksholm.fi>
2019-10-11 15:52:20 +0300

    add LOGIC.board

--- src/io.js
+++ src/io.js
@@ -5,6 +5,7 @@ import {AO} from './ao';
 import {AI} from './ai';
 import {TH} from './th';
 import {HP} from './hp';
+import {LOGIC} from './logic';
 
 export const IO = {};
 
@@ -13,8 +14,11 @@ IO.initial = board => {
   console.log("Start initialising io's");
   DO.initial(board);
   DI.initial(board);
-  AO.initial(board);
+//  AO.initial(board); // not need at the moment
   AI.initial(board);
   TH.initial(board);
   HP.initial(board);
+
+  // some other fixes
+  if(LOGIC.board === null) LOGIC.board = board;
 };
