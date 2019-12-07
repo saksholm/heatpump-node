f56884f5a8ab0ab2e036ac7eb64192626ba6e0c0
Joni Saksholm <joni@saksholm.fi>
2019-12-08 00:10:19 +0200

    added HP defrost logic

--- src/logic.js
+++ src/logic.js
@@ -75,6 +75,37 @@ LOGIC.loop = () => {
     }
 
 
+    // DEFROST
+    //TOOD: prevent this to run every run...
+    if(HP.defrost === false && AI.condenserPde.value >= AI.condenserPde.defrostPa ) {
+      // condenser Pa is over melting Pa limit
+      HP.defrost = true;
+      HP.mode = 'defrost';
+      HP.mqttStatus('modeChange');
+      // increase fanSpeed ?
+      if(DO.hpFanOutput.value < DO.hpFanOutput.maxValue) {
+        DO.hpFanOutput.set(DO.hpFanOutput.value + 10); // increase fan speed by 5%
+      }
+
+      if(DO.hpFanOutput.value === DO.hpFanOutput.maxValue ) {
+        // cannot increase fanspeed in given bounds.. lower hpOutput
+        DO.hpOutput.set(DO.hpOutput.set(DO.hpOutput.value - 10)); // decrease hpoutput by 5%
+      }
+
+    }
+
+    if(HP.defrost === true && AI.condenserPde.value <= AI.condenserPde.cleanPa) {
+      HP.defrost = false;
+      HP.mode = 'heating';
+      HP.mqttStatus('modeChange');
+
+      DO.hpFanOutput.set(DO.hpFanOutput.value - 10);
+      DO.hpOutput.set(DO.hpOutput.value + 10);
+
+    }
+
+
+
 
 
 
