15345648278f7f36b6606c81e89bf94c7695e6d7
Joni Saksholm <joni@saksholm.fi>
2020-05-30 20:23:49 +0300

    improvements

--- src/hp.stop.js
+++ src/hp.stop.js
@@ -8,9 +8,13 @@ import {
 } from './func';
 
 export const hpStop = function(emergency=false) {
+  HP.mode = 'stopping';
+  HP.allowedToRun = false;
+  console.log("HP allowedToRun false");
+
   if(emergency) {
     HP.emergencyShutdown = true;
-    HP.restartTimestamp = unixtimestamp();
+
     console.log("\n**********************************************");
     console.log("**                                          **");
     console.log("**          EMERGENCY SHUTDOWN              **");
@@ -22,13 +26,16 @@ export const hpStop = function(emergency=false) {
   const {wait} = HP.board;
   // let's check if minimumRunningTime is enough... if not.. we run it to end.
   // on emergency case we want to stop immediately.
-  wait(emergency ? 0 : calculateTimeout(HP.actualRunStartTimestamp, HP.minimumRunningTime, true), () => {
-    HP.mode = 'stop';
-    console.log("HP mode 'stop'");
-    HP.allowedToRun = false;
-    console.log("HP allowedToRun false");
+  const calculatedTimeoutMillis = calculateTimeout(HP.actualRunStartTimestamp, HP.minimumRunningTime, true);
+  console.log("STOPPING in...... calculatedTimeoutMillis", calculatedTimeoutMillis, "emergency: ", emergency);
 
-    DO.hpOutput.set(0); // shutdown output
+  wait(emergency ? 0 : calculatedTimeoutMillis, () => {
+    HP.mode = 'stop';
+    HP.restartTimestamp = unixtimestamp();
+
+    console.log("HP mode 'stop'");
+
+    DO.hpOutput.shutdown(); // shutdown output
     console.log("hpOutput 0");
     DO.hpAllowed.set("off");
     console.log("hpAllowed output off");
@@ -39,7 +46,7 @@ export const hpStop = function(emergency=false) {
     wait(20000,() => {
       DO.waterpumpCharging.output.off(); // waterpump charging relay to on
       console.log("waterpump charging output off()");
-      DO.load2Way.set(0); // let's open 2way valve 0%
+      DO.load2Way.shutdown(); // let's open 2way valve 0%
       console.log("load 2-way to 0%");
 
       // turn pid controller target to 0
@@ -50,20 +57,23 @@ export const hpStop = function(emergency=false) {
       // and open convection damper
       console.log("\nWait 10s more...\n");
       wait(10000, () => {
-        DO.hpFanOutput.set(0); // hp fan output to 0%
+        DO.hpFanOutput.shutdown(); // hp fan output to 0%
         console.log("hp fan output to 0%");
 
         DO.hpFan.output.off(); // Fan on
-        console.log("hp fan output off()");
+        console.log("hp fan off()");
 
         DO.damperConvection.output.on();
-        console.log("damper convection output on()");
+        console.log("damper convection on()");
         DO.damperOutside.output.off();
-        console.log("damper outside output off()");
+        console.log("damper outside off()");
 
 
-        console.log("\nStopped whole process\n");
-        console.info("To run process again please type: 'emergencyReset()'");
+        if(HP.emergencyShutdown) {
+          console.log("\nStopped whole process\n");
+          console.info("To run process again please type: 'emergencyReset()'");
+        }
+
       });
 
     });
