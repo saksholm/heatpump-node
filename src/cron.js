8ee8acb6302fee3aa80b5e0b81380c5dcfb7dcb1
Joni Saksholm <joni@saksholm.fi>
2020-10-09 17:53:58 +0300

    lcd stuff

--- src/cron.js
+++ src/cron.js
@@ -1,10 +1,12 @@
-import {GLOBALS} from './globals';
+//import {GLOBALS} from './globals';
 import {LCD} from './lcd';
 
 setTimeout(() => {
-  // reset lcd screen
-  LCD.board.loop(LCD.screen.autoReset, () =>{
-    LCD.screen.initial();
-    LCD.screen.currentInstance();
-  });
+  // reset lcd screen if screen is active
+  if(LCD.screen.active) {
+    LCD.board.loop(LCD.screen.autoReset, () =>{
+      LCD.screen.initial();
+      LCD.screen.currentInstance();
+    });
+  }
 },10000);
