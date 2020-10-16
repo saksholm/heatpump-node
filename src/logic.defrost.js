80b185e363541eaefff20dca38a5e57a1f18f0b9
Joni Saksholm <joni@saksholm.fi>
2020-10-16 12:16:52 +0300

    Update logic.defrost.js

--- src/logic.defrost.js
+++ src/logic.defrost.js
@@ -1,15 +1,15 @@
-import {GLOBALS} from './globals';
+//import {GLOBALS} from './globals';
 import {HP} from './hp';
 import {DO} from './do';
-import {TH} from './th';
-import {DI} from './di';
-import {AO} from './ao';
+//import {TH} from './th';
+//import {DI} from './di';
+//import {AO} from './ao';
 import {AI} from './ai';
 
 export const defrostLogic = () => {
 
   // DEFROST
-  //TOOD: prevent this to run every run...
+  //TODO: prevent this to run every run...
   if(HP.defrost === false && AI.condenserPde.value >= AI.condenserPde.defrostPa ) {
     // condenser Pa is over melting Pa limit
     HP.defrost = true;
@@ -21,8 +21,8 @@ export const defrostLogic = () => {
     }
 
     if(DO.hpFanOutput.value === DO.hpFanOutput.maxValue ) {
-      // cannot increase fanspeed in given bounds.. lower hpOutput
-      DO.hpOutput.set(DO.hpOutput.set(DO.hpOutput.value - 10)); // decrease hpoutput by 5%
+      // cannot increase fan speed in given bounds.. lower hpOutput
+      DO.hpOutput.set(DO.hpOutput.set(DO.hpOutput.value - 10)); // decrease hpOutput by 5%
     }
 
   }
