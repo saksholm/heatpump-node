4bec9f400f4fb7a4291f7a18884b87a0685f687e
Joni Saksholm <joni@saksholm.fi>
2020-12-17 20:45:04 +0200

    test different way to allow increase hpOutput different way

--- src/logic.hpOutputWatch.js
+++ src/logic.hpOutputWatch.js
@@ -66,9 +66,9 @@ export const logicHpOutputWatch = () => {
         'manual',
       ].includes(HP.mode)) {
         // if load2Way is <65 and hpOutput is not maxValue
-        if(DO.load2Way.value < 50 && DO.hpOutput.value < DO.hpOutput.maxValue) {
+        if(DO.load2Way.value > 40 && DO.hpOutput.value < DO.hpOutput.maxValue) {
           DO.hpOutput.increase(1);
-          GLOBALS.debugLevels.hpOutput && console.log(`HP.loop :: increasing hpOutput (to ${DO.hpOutput.value}) by 1% because load2Way (${DO.load2Way.value}) is less than 50%, HP.mode = ${HP.mode}`);
+          GLOBALS.debugLevels.hpOutput && console.log(`HP.loop :: increasing hpOutput (to ${DO.hpOutput.value}) by 1% because load2Way (${DO.load2Way.value}) is bigger than 40% (reverted.. 100% is closed), HP.mode = ${HP.mode}`);
         }
       }
 
