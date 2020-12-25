30d8e4c319436d4e4f9711fcfdfbdc237fc5714c
Joni Saksholm <joni@saksholm.fi>
2020-12-25 18:05:46 +0200

    start defrost only when hp4way value is heating and otherwise cut hpOutput by half

--- src/hp.hotgasWatch.js
+++ src/hp.hotgasWatch.js
@@ -27,36 +27,42 @@ export const hotgasWatch = () => {
     if(TH.hotgas.value > (HP.maxHotgas + deadZone)) {
       console.log("HP hotgas is maybe little bit too hot...");
 
+      // start defrosting only when heating mode and hotgas triggered
+      if(DO.hp4Way.value === 'heating') {
+        stopToDefrostAndContinue('HOTGAS_DEFROST');
+      } else {
 
-      stopToDefrostAndContinue('HOTGAS_DEFROST');
-/*
+        // too hot hotgas! drop output demand by half
+        if(DO.hpOutput.value > DO.hpOutput.minValue) {
+          let cutOutputToHalf = Math.round(DO.hpOutput.value / 2);
 
-      // too hot hotgas! drop output demand by half
-      if(DO.hpOutput.value > DO.hpOutput.minValue) {
-        let cutOutputToHalf = Math.round(DO.hpOutput.value / 2);
+          // if cutOutputToHalf is under minValue... set it to minValue
+          if(cutOutputToHalf < DO.hpOutput.minValue) cutOutputToHalf = DO.hpOutput.minValue;
 
-        // if cutOutputToHalf is under minValue... set it to minValue
-        if(cutOutputToHalf < DO.hpOutput.minValue) cutOutputToHalf = DO.hpOutput.minValue;
+          DO.hpOutput.set(cutOutputToHalf);
 
-        DO.hpOutput.set(cutOutputToHalf);
+          console.log("... cut hp output demand to half (or at least to minimum)");
+        }
 
-        console.log("... cut hp output demand to half (or at least to minimum)");
+        /*
+        NOTE: not relevant anymore because PID handles
+
+        if(DO.load2Way.value < DO.load2Way.maxValue) {
+          let doubleOutput = Math.round(DO.load2Way.value * 2);
+          // check if doubled valve opening is over maximum... then set it to maximum
+          if(doubleOutput > DO.load2Way.maxValue) doubleOutput = DO.load2Way.maxValue;
+
+          DO.load2Way.set(doubleOutput);
+          console.log("... and doubled load 2-way valve output (or at least to maximum)");
+        }
+
+         */
+
+        // should put next legit check time?! so this have time to stabilize things
+        // for 15s?
+        HP.nextLoopIntervalTimestamps.hotgas = timestamp + HP.hotgasWatchInterval;
+
       }
-
-      if(DO.load2Way.value < DO.load2Way.maxValue) {
-        let doubleOutput = Math.round(DO.load2Way.value * 2);
-        // check if doubled valve opening is over maximum... then set it to maximum
-        if(doubleOutput > DO.load2Way.maxValue) doubleOutput = DO.load2Way.maxValue;
-
-        DO.load2Way.set(doubleOutput);
-        console.log("... and doubled load 2-way valve output (or at least to maximum)");
-      }
-      // should put next legit check time?! so this have time to stabilize things
-      // for 15s?
-      HP.nextLoopIntervalTimestamps.hotgas = timestamp + HP.hotgasWatchInterval;
-
-
-      */
     }
   }
 };
