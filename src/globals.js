eb336aac9860addeb54c41e77010aaa4ffab770d
Joni Saksholm <joni@saksholm.fi>
2021-01-27 13:17:49 +0200

    increase boiler upper soft minimum

--- src/globals.js
+++ src/globals.js
@@ -45,7 +45,7 @@ const GLOBALS = {
     upper: { // middle upper!!
       request: false,
       demand: true,
-      softMinimum: 30.0,
+      softMinimum: 32.0,
       softMaximum: 40.0,
       hardMinimum: 25.0,
       hardMaximum: 60.0,
