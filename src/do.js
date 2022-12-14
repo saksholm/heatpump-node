dfbbcc0fe7c8020d1b6c7aa97db6c232bdd4da93
Joni Saksholm <joni@saksholm.fi>
2022-12-14 20:21:47 +0200

    smaller max value for load2way

--- src/do.js
+++ src/do.js
@@ -429,7 +429,7 @@ export const DO = {
     value: 0,
     minValue: 5, //TODO: should check what is the real minimum to use
     maxValue: 100,
-    maxValueOnRunning: 83,
+    maxValueOnRunning: 78,
     maxValueOnDefrost: 80,
     target: null,
     manualMode: false,
