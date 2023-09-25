201761fb348bba7b4e8b6bea33ec7369e5e5e7bd
Joni Saksholm <joni@saksholm.fi>
2023-09-26 00:20:09 +0300

    Update func.js

--- src/func.js
+++ src/func.js
@@ -100,8 +100,11 @@ export const genericInitial = (module, name, board, callback=null) => {
       if(instance?.mqttState?.length > 0) {
         mqttPublish(module.board.mqttClient, instance.mqttState, instance.value);
       }
+
       if(typeof instance?.mqttExtraStates?.length > 0) {
+        console.log("MQTT Publishes for module", name);
         instance?.mqttExtraStates.forEach(mqttObj => {
+          console.log("  :: MQTT extra publishes topic:", mqttObj.topic);
           mqttPublish(module.board.mqttClient, mqttObj.topic, mqttObj.value);
         });
       }
