7639462c92102faa87a3a688bb7d1cc100ef219f
Joni Saksholm <joni@saksholm.fi>
2025-08-06 00:31:48 +0300

    raise hxOutTarget to 46

--- src/hp.js
+++ src/hp.js
@@ -54,7 +54,7 @@ export const HP = {
   maxHotgas: 80, // 80c
   maxHotgasEmergency: 90,
   hxInMaximum: 35,
-  hxOutTarget: 44, // HP TARGET OUTPUT
+  hxOutTarget: 46,//44, // HP TARGET OUTPUT
   cooling: {
     minAhuTemp: 13,
     maxAhuTemp: 18,
