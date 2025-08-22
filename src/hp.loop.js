a26be376eda0c62bfa1d8da5549a4ad026715df0
Joni Saksholm <joni@saksholm.fi>
2025-08-22 15:33:55 +0300

    prettier

--- src/hp.loop.js
+++ src/hp.loop.js
@@ -1,22 +1,21 @@
-import {HP} from './hp';
-import {DO} from './do';
-import {GLOBALS} from './globals';
-import {hotgasWatch} from './hp.hotgasWatch';
-import {hpCoolingLoop} from './hp.cooling';
-import {hpHeatingLoop} from './hp.heating';
-import {hpWaterHeating} from './hp.waterHeating';
+import { HP } from './hp';
+import { DO } from './do';
+import { GLOBALS } from './globals';
+import { hotgasWatch } from './hp.hotgasWatch';
+import { hpCoolingLoop } from './hp.cooling';
+import { hpHeatingLoop } from './hp.heating';
+import { hpWaterHeating } from './hp.waterHeating';
 
-import {logicAlarms} from './logic.alarms';
-import {logicLoad2WayController} from './logic.load2way';
-import {logicHxIn} from './logic.hxIn';
-import {logicHpOutputWatch} from './logic.hpOutputWatch';
-import {logicHpFanOutputWatch} from './logic.hpFanOutputWatch';
-import {dynamicHpOutputMax} from './logic.hpOutputDynamicMax';
+import { logicAlarms } from './logic.alarms';
+import { logicLoad2WayController } from './logic.load2way';
+import { logicHxIn } from './logic.hxIn';
+import { logicHpOutputWatch } from './logic.hpOutputWatch';
+import { logicHpFanOutputWatch } from './logic.hpFanOutputWatch';
+import { dynamicHpOutputMax } from './logic.hpOutputDynamicMax';
 
-import {de} from "date-fns/locale";
 
 export const hpLoop = () => {
-  HP.board.loop(GLOBALS.logicLoopInterval,() => {
+  HP.board.loop(GLOBALS.logicLoopInterval, () => {
     // hp allowed to run.. change max hp output value dynamically
     dynamicHpOutputMax();
 
@@ -27,7 +26,7 @@ export const hpLoop = () => {
     // TBD: this switch is just a mock.. depend on HP.mode what to do
 
     // TODO: change to HP.program...!! different than mode
-    switch(HP.mode) {
+    switch (HP.mode) {
       case "cooling":
         // call cooling func
         hpCoolingLoop();
@@ -45,10 +44,10 @@ export const hpLoop = () => {
 
   });
 
-  HP.board.loop(GLOBALS.logicLoopLoad2WayLoopInterval,() => {
+  HP.board.loop(GLOBALS.logicLoopLoad2WayLoopInterval, () => {
     // this logic is only for heat to water....
     // boiler logic asking heat to water
-    if(GLOBALS.dryRun || (GLOBALS.heatToWater && DO.load2Way.value !== 0)) {
+    if (GLOBALS.dryRun || (GLOBALS.heatToWater && DO.load2Way.value !== 0)) {
 
       logicLoad2WayController();
       logicHxIn(); // stops when minValue is exceeded
